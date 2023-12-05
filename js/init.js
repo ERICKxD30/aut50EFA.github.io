2eba2fe80bb1aeaf93f07b1955b70b5c6930f6e6
ERICKxD30 <152240142+ERICKxD30@users.noreply.github.com>
2023-12-04 19:37:32 -0600

    Add files via upload

---
+++ js/init.js
@@ -0,0 +1,11 @@
+/*  Initializa Firebase con la configuración del proyecto. Revisa la
+ * configuración en tu servidor de Firebase.  */
+// @ts-ignore
+firebase.initializeApp({
+  apiKey: "AIzaSyCnMitQqY4z6Op2lWLaInx_jayo6MKTBaQ",
+  authDomain: "primerproyecto-a27f7.firebaseapp.com",
+  projectId: "primerproyecto-a27f7",
+  storageBucket: "primerproyecto-a27f7.appspot.com",
+  messagingSenderId: "1003780326371",
+  appId: "1:1003780326371:web:520b138322c089572f96d6"
+    });
