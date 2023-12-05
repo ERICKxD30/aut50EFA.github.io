11f647e50e25bd6d7e2edb62e685c8d6989c44de
ERICKxD30 <152240142+ERICKxD30@users.noreply.github.com>
2023-12-04 19:46:52 -0600

    Update init.js

--- js/init.js
+++ js/init.js
@@ -2,10 +2,10 @@
  * configuración en tu servidor de Firebase.  */
 // @ts-ignore
 firebase.initializeApp({
-  apiKey: "AIzaSyCnMitQqY4z6Op2lWLaInx_jayo6MKTBaQ",
-  authDomain: "primerproyecto-a27f7.firebaseapp.com",
-  projectId: "primerproyecto-a27f7",
-  storageBucket: "primerproyecto-a27f7.appspot.com",
-  messagingSenderId: "1003780326371",
-  appId: "1:1003780326371:web:520b138322c089572f96d6"
+    apiKey: "AIzaSyCnMitQqY4z6Op2lWLaInx_jayo6MKTBaQ",
+    authDomain: "primerproyecto-a27f7.firebaseapp.com",
+    projectId: "primerproyecto-a27f7",
+    storageBucket: "primerproyecto-a27f7.appspot.com",
+    messagingSenderId: "1003780326371",
+    appId: "1:1003780326371:web:520b138322c089572f96d6"
     });
